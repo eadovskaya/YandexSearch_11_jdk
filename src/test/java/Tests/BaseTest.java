c37a844f2558e54a19b0f91d32f42216bedf8e9e
eadovskaya <elenasadovskaya1993@gmail.com>
2022-04-11 21:08:29 +0300

    Added Remote Driver

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -1,5 +1,7 @@
 package Tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static BaseSettings.Login.*;
@@ -7,6 +9,12 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class BaseTest {
     String baseUrl = "https://yandex.ru/search";
+
+    @BeforeClass
+    public static void setUp() {
+        //Configuration.driverManagerEnabled = false;
+        //Configuration.remote = "http://localhost:4444/wd/hub";
+    }
 
     @Test
     public void Auth(){
